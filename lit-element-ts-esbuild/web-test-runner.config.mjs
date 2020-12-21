833c4593ee6a740dca30d626b2b2e233c20566b0
Javier <javier.garcia@meteologica.com>
2020-12-21 16:59:00 +0100

    feat: fix test config and use pnpm

--- lit-element-ts-esbuild/web-test-runner.config.mjs
+++ lit-element-ts-esbuild/web-test-runner.config.mjs
@@ -2,5 +2,6 @@ import { esbuildPlugin } from "@web/dev-server-esbuild";
 
 export default {
   preserveSymlinks: true,
+  nodeResolve: { dedupe: ['lit-html'] },
   plugins: [esbuildPlugin({ ts: true })],
 };
